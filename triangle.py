203dac61e83b1aa982bd8f5ddc6fbc72bfa8ec86
yusupovna1403 <yusupovna2003girl@gmail.com>
2023-01-28 09:40:20 +0500

    triangle

--- triangle.py
+++ triangle.py
@@ -1,7 +1,17 @@
-from polygon import Polygon
-
-class Triangle(Polygon):
+from math import*
+class Triangle:
     def __init__(self, a, b, c):
         self.a = a
         self.b = b
         self.c = c
+    def getPerimeter(self):
+        return self.a + self.b + self.c
+    
+    def getArea(self):
+        k = self.getPerimeter()
+        area = sqrt(k/2*(k/2 - self.a)*(k/2 - self.b)*(k/2 - self.c))
+        return area
+
+obj = Triangle(3,4,5)
+print(obj.getPerimeter())
+print(obj.getArea())
